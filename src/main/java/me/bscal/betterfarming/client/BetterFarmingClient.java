88dff8ddbcab106cd4d99ba6c3bbc3125abf6f56
bscal <jonfus123@hotmail.com>
2021-07-20 23:30:51 -0400

    Started for fixes for BiomeEffects

--- src/main/java/me/bscal/betterfarming/client/BetterFarmingClient.java
+++ src/main/java/me/bscal/betterfarming/client/BetterFarmingClient.java
@@ -5,19 +5,35 @@ import me.bscal.betterfarming.client.seasons.biome.BiomeEffectHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.GrassColors;
 
 @Environment(EnvType.CLIENT) public class BetterFarmingClient implements ClientModInitializer
 {
 
-	private final BiomeEffectHandler m_effectHandler = new BiomeEffectHandler();
+	public static BiomeEffectHandler m_effectHandler;
 
 	@Override
 	public void onInitializeClient()
 	{
 		ClientPlayNetworking.registerGlobalReceiver(BetterFarming.SYNC_PACKET, m_effectHandler.SyncTime());
-		ClientPlayNetworking.registerGlobalReceiver(BetterFarming.SEASON_PACKET,
-				m_effectHandler.SyncSeasonChange());
+		//ClientPlayNetworking.registerGlobalReceiver(BetterFarming.SEASON_PACKET,
+		//m_effectHandler.SyncSeasonChange());
 		//m_effectHandler.UpdateSeasonColors();
+
+		ClientTickEvents.END_WORLD_TICK.register((world -> {
+			if (m_effectHandler.reloadWorld)
+			{
+				m_effectHandler.UpdateSeasonColors();
+				MinecraftClient.getInstance().worldRenderer.reload();
+				m_effectHandler.reloadWorld = false;
+			}
+		}));
 	}
 }
--- src/main/java/me/bscal/betterfarming/client/seasons/biome/BiomeEffectHandler.java
+++ src/main/java/me/bscal/betterfarming/client/seasons/biome/BiomeEffectHandler.java
@@ -1,66 +1,58 @@
 package me.bscal.betterfarming.client.seasons.biome;
 
 import me.bscal.betterfarming.BetterFarming;
-import me.bscal.betterfarming.common.mixin.world.ClientWorldAccessor;
 import me.bscal.betterfarming.common.seasons.SeasonClock;
 import me.bscal.betterfarming.common.utils.Color;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.mixin.biome.modification.BiomeEffectsAccessor;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.color.world.BiomeColors;
-import net.minecraft.client.world.BiomeColorCache;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeEffects;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-@Environment(EnvType.CLIENT)
-public class BiomeEffectHandler
+@Environment(EnvType.CLIENT) public class BiomeEffectHandler
 {
 
+	public final Map<Biome, BiomeEffectChanger> m_biomeEffectChangerMap = new HashMap<>();
+	public boolean reloadWorld = false;
 	private final SeasonClock m_seasonClock = new SeasonClock();
 	private final List<BiomeEffectChanger> m_biomeEffectChangers = new ArrayList<>();
-	private boolean m_firstUpdate = true;
+	private final ClientWorld m_world;
 
-	public BiomeEffectHandler()
+	public BiomeEffectHandler(ClientWorld world)
 	{
+		m_world = world;
 		Register(BiomeChangers.PLAINS_CHANGER);
 	}
 
 	public void Register(BiomeEffectChanger changer)
 	{
+		changer.biome = m_world.getRegistryManager().get(Registry.BIOME_KEY).get(changer.key);
+		m_biomeEffectChangerMap.put(changer.biome, changer);
 		m_biomeEffectChangers.add(changer);
 	}
 
 	public ClientPlayNetworking.PlayChannelHandler SyncTime()
 	{
-		return (client, handler, buf, responseSender) ->
-		{
-			long ticks = buf.readLong();
-			int season = buf.readInt();
-			m_seasonClock.ticksSinceCreation = ticks;
-			m_seasonClock.currentSeason = season;
-			if (m_seasonClock.ticksSinceCreation % 100 == 0)
-			{
-				UpdateSeasonColors();
-				//MinecraftClient.getInstance().world.reloadColor();
-				((ClientWorldAccessor)MinecraftClient.getInstance().world).getColorCache().put(BiomeColors.GRASS_COLOR, new BiomeColorCache());
-			}
-		};
-	}
-
-	public ClientPlayNetworking.PlayChannelHandler SyncSeasonChange()
-	{
 		return (client, handler, buf, responseSender) -> {
-			m_seasonClock.currentSeason = buf.readInt();
-			UpdateSeasonColors();
+			if (client.world.isClient)
+			{
+				long ticks = buf.readLong();
+				int season = buf.readInt();
+				m_seasonClock.ticksSinceCreation = ticks;
+				m_seasonClock.currentSeason = season;
+				if (m_seasonClock.ticksSinceCreation % 100 == 0)
+				{
+					reloadWorld = true;
+				}
+			}
 		};
 	}
 
@@ -77,13 +69,17 @@ public class BiomeEffectHandler
 	{
 
 		public final RegistryKey<Biome> key;
-		public final Biome biome;
+		public Biome biome;
 		public int[] grassColors;
 
 		public BiomeEffectChanger(RegistryKey<Biome> key)
 		{
 			this.key = key;
-			this.biome = BuiltinRegistries.BIOME.get(key);
+		}
+
+		public void SetBiome(Biome worldBiome)
+		{
+			this.biome = worldBiome;
 		}
 
 		public BiomeEffectChanger SetGrassColors(int[] grassColors)
@@ -96,10 +92,7 @@ public class BiomeEffectHandler
 		{
 			BiomeEffects effects = biome.getEffects();
 			BiomeEffectsAccessor effectsAccessor = (BiomeEffectsAccessor) effects;
-			effectsAccessor.fabric_setGrassColor(Optional.of(new Color(255, 0, 0, 0).toInt()));
-			BetterFarming.LOGGER.info(effects.getGrassColor());
-			BetterFarming.LOGGER.info(biome.getGrassColorAt(0,0));
-			BetterFarming.LOGGER.info(new Color(grassColors[0]).toInt());
+			effectsAccessor.fabric_setGrassColor(Optional.of(new Color(255, 0, 0, 255).toInt()));
 		}
 
 		private Optional<Integer> CreateOptionalFromArray(int[] array, int season)
