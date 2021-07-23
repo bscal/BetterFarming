0ab6fc8e6b70bcc3fcd1b5178c7f5ca964121f81
bscal <jonfus123@hotmail.com>
2021-07-22 23:07:02 -0400

    Added simple biome changer

--- src/main/java/me/bscal/betterfarming/client/seasons/biome/BiomeChangers.java
+++ src/main/java/me/bscal/betterfarming/client/seasons/biome/BiomeChangers.java
@@ -1,23 +1,50 @@
 package me.bscal.betterfarming.client.seasons.biome;
 
-import me.bscal.betterfarming.mixin.client.biome.BiomeInvoker;
 import me.bscal.betterfarming.common.utils.Color;
+import me.bscal.betterfarming.mixin.client.biome.BiomeInvoker;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
 
-@Environment(EnvType.CLIENT)
-public final class BiomeChangers
+@Environment(EnvType.CLIENT) public final class BiomeChangers
 {
+
+	public static class SimpleBiomeChanger extends BiomeChanger
+	{
+		public SimpleBiomeChanger(RegistryKey<Biome> key, ClientWorld world)
+		{
+			super(key, world);
+			int defaultColor = biome.getEffects()
+					.getGrassColor()
+					.orElse(((BiomeInvoker) (Object) biome).invokeGetDefaultGrassColor());
+
+			Color spring = new Color(defaultColor);
+			spring.saturate(20);
+			spring.lighten(20);
+			grassColors[0] = spring.toInt();
+
+			grassColors[1] = defaultColor;
+
+			Color fall = new Color(defaultColor);
+			fall.saturate(-5);
+			fall.lighten(-20);
+			grassColors[2] = fall.toInt();
+
+			Color winter = new Color(defaultColor);
+			winter.saturate(-30);
+			winter.lighten(-30);
+			grassColors[3] = winter.toInt();
+		}
+	}
 
 	public static class PlainsChanger extends BiomeChanger
 	{
 		public PlainsChanger(ClientWorld world)
 		{
 			super(BiomeKeys.PLAINS, world);
-
-
 			grassColors = new int[4];
 
 			int tmpInt = biome.getEffects()
